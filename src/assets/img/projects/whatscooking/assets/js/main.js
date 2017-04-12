6e7661209e8e6706f4dc69a9ec6d5b791679a72e
Muhammed Anwar <muhammedshabbeer@hotmail.com>
2017-04-12 18:21:35 -0400

    Added TSNE Animation

--- src/assets/img/projects/whatscooking/assets/js/main.js
+++ src/assets/img/projects/whatscooking/assets/js/main.js
@@ -39,10 +39,13 @@ var scene, camera, renderer;
 // Data Points
 var pointField;
 var cuisineField;
+var boundingBox;
 var PARTICLE_SIZE = 7;
 var ingredientLabel = [];
-var animationData =[];
+var animationData =null;
 var num_cuisines;
+var cuisines ={};
+var dirty = false;
 
 //Interaction
 var raycaster, intersects;
@@ -50,7 +53,7 @@ var mouse, INTERSECTED;
 var UI;
 var animate = false;
 var t = 0;
-var dir = 0.015;
+var dir = 0.2;
 
 //Mouse vars
 var clicked;
@@ -76,27 +79,38 @@ function getColor(cuisine_class, num_cuisines){
 function populate_data(data){
   var data_points = data.split("\n");
   var dataGeometry = new THREE.Geometry();
-  num_cuisines = -Infinity;
+  num_cuisines = 0;
+  cuisines={};
 
   for(var i =0; i <data_points.length; i++){
     var comps = data_points[i].split(',');
     
-    var x = parseFloat(comps[0])*10;
-    var y = parseFloat(comps[1])*6;
-    if(comps.length==4){
-      var z =0;
-      var cuisine = parseInt(comps[2]);
-      var ingredient = comps[3];
-    }else if (comps.length==5){
-      var z =parseFloat(comps[2])*10;
-      var cuisine = parseInt(comps[3]);
-      var ingredient = comps[4];
+    // var x = parseFloat(comps[0])*10;
+    // var y = parseFloat(comps[1])*6;
+    // if(comps.length==4){
+    //   var z =0;
+    //   var cuisine = parseInt(comps[2]);
+    //   var ingredient = comps[3];
+    // }else if (comps.length==5){
+    //   var z =parseFloat(comps[2])*10;
+    //   var cuisine = parseInt(comps[3]);
+    //   var ingredient = comps[4];
+    // }
+    var ingredient = comps[0];
+    var cuisine = comps[1];
+    var x = parseFloat(comps[2])*10;
+    var y =  parseFloat(comps[3])*6;
+    var z = 0;
+    if(comps.length==5){
+      z =  parseFloat(comps[4])*10;
     }
-
-    ingredientLabel.push({"ingredient":ingredient, "cuisine":cuisine});
-    if(cuisine > num_cuisines){
-      num_cuisines = cuisine;
+    
+    ingredientLabel.push({"ingredient":ingredient, "cuisine":cuisine});    
+    if(!(cuisine in cuisines)){      
+      cuisines[cuisine]=num_cuisines;
+      num_cuisines++;
     }
+    
     var point = new THREE.Vector3(x,y,z);
     dataGeometry.vertices.push(point);
   }
@@ -105,30 +119,28 @@ function populate_data(data){
   var colors = [];  
   for(var i =0; i <data_points.length; i++){
     var comps = data_points[i].split(',');
-    if(comps.length==4){
-      var cuisine = parseInt(comps[2]);
-    }else if(comps.length ==5){
-      var cuisine = parseInt(comps[3]);
-    }
-    colors[i] = getColor(cuisine, num_cuisines)[0];
+    var cuisine = comps[1];    
+    colors[i] = getColor(cuisines[cuisine], num_cuisines)[0];
   }
   dataGeometry.colors = colors;
 
   //Populate Legend:
   var legend = $("#legend-list");
   var html="";
-  for(var i =0;i<num_cuisines;i++){
-    var hsl_color = getColor(i,num_cuisines)[1];
-    html+="<li> <div class=\"box\" style=\"background:" + hsl_color + "\"></div>" + i + "</li>"
+  for (var key in cuisines) {
+    if (cuisines.hasOwnProperty(key)) {
+      var hsl_color = getColor(cuisines[key],num_cuisines)[1];
+      html+="<li> <div class=\"box\" style=\"background:" + hsl_color + "\"></div>" + key + "</li>"
+    }
   }
+
   legend.html(html);
 
   var pointTexture = THREE.ImageUtils.loadTexture('assets/img/texture/particle.png');
   var pointMaterial = new THREE.PointsMaterial( { map:pointTexture, size:PARTICLE_SIZE, transparent: false, alphaTest:0.5, vertexColors: THREE.VertexColors, depthTest:false });
   pointField = new THREE.Points( dataGeometry, pointMaterial );
-  scene.add( pointField );
+  scene.add( pointField ); 
 }
-
 /***********************
  * Rendering Functions *
  ***********************/
@@ -174,16 +186,36 @@ function renderScene() {
 
 function updateScene() {
   controls.update();
+  //scene.remove(boundingBox);
+  boundingBox = new THREE.BoxHelper(pointField, 0xff0000);  
+  //scene.add(boundingBox);
 
-  if(animate){
+  var box3 = new THREE.Box3();
+  box3.setFromObject( boundingBox )
+
+  
+  //console.log(boundingBox);
+  var height = box3.getSize().y;  
+  var dist = 200;
+  camera.fov=2 * Math.atan( height / ( 2 * dist ) ) * ( 180 / Math.PI );
+  camera.updateProjectionMatrix();
+
+  if(animate &&animationData){
     var geometry = pointField.geometry;
     var vertices = geometry.vertices;
 
+
+    
     var frameStart = parseInt(Math.floor(t) % animationData.length);
     var frameEnd = parseInt(Math.floor(t+1) % animationData.length);
     var time = t - Math.floor(t);
     for(var i =0; i <vertices.length;i++){
       vertices[i].lerpVectors(animationData[frameStart][i],animationData[frameEnd][i], time);
+    }    
+   
+    if(frameEnd == animationData.length-1 && time > 0.8){      
+      animate = !animate;
+      t =0;
     }
     
     t+=dir;  
@@ -245,10 +277,10 @@ function onDocumentMouseClick( event ) {
     console.log(cur_cuisine);
 
     var pointTexture = THREE.ImageUtils.loadTexture('assets/img/texture/particle.png');
-    var pointMaterial = new THREE.PointsMaterial( { map:pointTexture, size:PARTICLE_SIZE*1.50, transparent: false, alphaTest:0.5, color: getColor(cur_cuisine,num_cuisines)[1], depthTest:false });
+    var pointMaterial = new THREE.PointsMaterial( { map:pointTexture, size:PARTICLE_SIZE*1.50, transparent: false, alphaTest:0.5, color: getColor(cuisines[cur_cuisine],num_cuisines)[1], depthTest:false });
     cuisineField = new THREE.Points( cuisineGeometry, pointMaterial );
     scene.add( cuisineField );
-    pointField.material.opacity = 0.57;
+    pointField.material.opacity = 0.6;
   }else{
     scene.remove(cuisineField);
     pointField.material.opacity = 1.0;
@@ -284,7 +316,7 @@ function initializeScene() {
   // Camera and set initial view
   var aspectRatio  = canvasWidth/canvasHeight;
   camera = new THREE.PerspectiveCamera( CAMERA.fov, aspectRatio, CAMERA.near, CAMERA.far );
-  camera.position.set( CAMERA.zoomX, CAMERA.zoomY, CAMERA.zoomZ );
+  camera.position.set( CAMERA.zoomX, CAMERA.zoomY, CAMERA.zoomZ );  
   camera.lookAt(scene.position);
   scene.add(camera);
 
@@ -340,32 +372,25 @@ function processData(data){
 
 function loadAnimationData(data){
   var data_points = data.split("\n");
- 
-  var frame_list =[];
-  for (var j =0; j<2;j++){
-    if(j==0){
-      for(var i =0; i <data_points.length; i++){
-        var comps = data_points[i].split(',');
-        var x = parseFloat(comps[0])*10;
-        var y = parseFloat(comps[1])*6;    
-        var point = new THREE.Vector3(x,y,0);
-        frame_list.push(point);
-      }
-    }
-    if(j==1){
-      for(var i =0; i <data_points.length; i++){
-        var comps = data_points[i].split(',');
-        var x = parseFloat(comps[0])*10;
-        var y = parseFloat(comps[1])*6; 
-        var z = parseFloat(comps[2])*10;
-        
-        var point = new THREE.Vector3(x,y,z);
-        frame_list.push(point);
-      }
-    }
-    animationData.push(frame_list);
-    frame_list = [];
+  var frames = (data_points[0].split(',').length-2)/2;
+  console.log("Loading " + frames + " frames of animation");
+
+  animationData = new Array();
+  for(var i =0; i < frames;i++){
+    animationData[i] = new Array(data_points.length);
   }
+  for(var i =0;i < data_points.length;i++){
+    var line = data_points[i].split(',');  
+    
+    for(var frame=0; frame < frames; frame++){
+      var x = parseFloat(line[2+(frame*2)])*10;
+      var y = parseFloat(line[2+(frame*2)+1])*6;
+      var z =0;
+      animationData[frame][i] = new THREE.Vector3(x,y,z);
+    }   
+  
+  } 
+  
   
 }
 $(document).ready(function() {
@@ -374,7 +399,7 @@ $(document).ready(function() {
   
   $.ajax({
         type: "GET",
-        url: "assets/data-tsne-2D.csv",
+        url: "assets/2D_tsne_word_data.txt",
         dataType: "text",
         success: function(data) {processData(data);}
      });
@@ -382,26 +407,42 @@ $(document).ready(function() {
 
 //UI interaction functions:
 function load_2D_data(){
+  dirty=true;
   scene.remove(pointField);
   t=0;
   $.ajax({
         type: "GET",
-        url: "assets/data-tsne-2D.csv",
+        url: "assets/2D_tsne_word_data.txt",
         dataType: "text",
         success: function(data) {populate_data(data);}
      });
 }
 function load_3D_data(){
+  dirty = true;
   scene.remove(pointField);
   t=0;
   $.ajax({
         type: "GET",
-        url: "assets/data-tsne-3D.csv",
+        url: "assets/3D_tsne_word_data.txt",
         dataType: "text",
-        success: function(data) {loadAnimationData(data);populate_data(data);}
+        success: function(data) {populate_data(data);}
      });
 }
-function toggleAnimation(){   
+function toggleAnimation(){  
+  controls.reset();
   animate = !animate;
+  if(dirty){
+    load_2D_data();
+    dirty = false;
+  }
+  if(!animationData){
+    console.log("Reloading Animation Data");
+    $.ajax({
+        type: "GET",
+        url: "assets/2D_tsne_animate_data.txt",
+        dataType: "text",
+        success: function(data) {loadAnimationData(data);}
+     });    
+  }
 }
 
